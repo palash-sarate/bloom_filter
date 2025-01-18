07e0932efc8ac59ba7bbb3bc9180851fdc79121e
Palash <99325824+palash-sarate@users.noreply.github.com>
2025-01-18 15:13:31 +0530

    Add initial Bloom Filter implementation with Murmur3 hashing and configuration files

---
+++ go.mod
@@ -0,0 +1,5 @@
+module bloom_filter
+
+go 1.23.5
+
+require github.com/spaolacci/murmur3 v1.1.0 // indirect
--- main.go
+++ main.go
@@ -2,19 +2,44 @@ package main
 
 import (
 	"fmt"
+	"hash"
+	"github.com/spaolacci/murmur3"
 )
 
 type BloomFilter struct {
-	bitArray []bool
+	bitArray []bool 
+	hasher hash.Hash32
 }
 
 func NewBloomFilter(size int) *BloomFilter {
 	return &BloomFilter{
 		bitArray: make([]bool, size),
+		hasher: murmur3.New32(),
 	}
+}
+
+func (bf *BloomFilter) Add(data []byte) {
+	bf.hasher.Write(data)
+	hash := bf.hasher.Sum32()
+	bf.hasher.Reset()
+	fmt.Println(hash)
+}
+
+func (bf *BloomFilter) Contains(data []byte) bool{
+	bf.hasher.Write(data)
+	hash := bf.hasher.Sum32()
+	bf.hasher.Reset()
+	fmt.Println(hash)
+	return false
 }
 
 func main() {
 	bf := NewBloomFilter(10)
-	fmt.Println(bf)
+	init_strings := []string{"hello", "world", "foo", "bar", "baz"}
+	for _, s := range init_strings {
+		bf.Add([]byte(s))
+	}
+	for _, s := range init_strings {
+		bf.Contains([]byte(s))
+	}
 }
